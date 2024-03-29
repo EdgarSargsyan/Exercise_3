6cf6dc03a2ed64261f88d388c55a834e760445d5
EdgarSargsyan <sargsyanedg@gmail.com>
2024-03-29 10:49:38 +0000

    route 53 added

---
+++ route53.tf
@@ -0,0 +1,10 @@
+
+# Create Route 53 record pointing to the load balancer
+resource "aws_route53_record" "lb_dns_record" {
+  zone_id = "Z10299161M47DBD68JOWT"
+  name    = "babkenasoyan.com"   
+  type    = "CNAME"            
+  ttl     = "300"
+  records = [aws_lb.my_alb.dns_name]
+}
+
